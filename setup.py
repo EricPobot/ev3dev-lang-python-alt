cf576bdc4b939accaf68709468717c04362a359c
Eric Pascual <eric@pobot.org>
2015-11-08 02:22:12 +0100

    change version handlingin setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='python-ev3dev-ep',
+    version='0.1.0.post4',
+    description='Python language bindings for ev3dev',
+    author='Eric Pascual',
+    author_email='eric@pobot.org',
+    license='MIT',
+    url='https://github.com/rhempel/ev3dev-lang-python',
+    include_package_data=True,
+    packages=['ev3dev']
+    )
+
